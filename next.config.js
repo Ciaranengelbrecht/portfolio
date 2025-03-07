5c90fdccebe45638362916497196c13e9626c751
Ciaran Engelbrecht <131735166+Ciaranengelbrecht@users.noreply.github.com>
2025-03-07 15:18:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 // next.config.js
 module.exports = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
   images: {
     unoptimized: true,
   },
