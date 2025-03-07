c5a4d6721b2b769c1caafc7743b97333b84b88b4
Ciaran Engelbrecht <131735166+Ciaranengelbrecht@users.noreply.github.com>
2025-03-07 12:56:48 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+module.exports = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+};
