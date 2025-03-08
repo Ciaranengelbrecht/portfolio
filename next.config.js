5999fc55c98c1d203705f0a5e7c7fac0040ba2fa
ciaranengelbrecht <cizzaengel@gmail.com>
2025-03-08 10:59:37 +0800

    deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 // next.config.js
+const isCustomDomain = process.env.DEPLOY_ENV === "CUSTOM_DOMAIN";
+
 module.exports = {
   output: "export", // Enable static export
   images: {
     unoptimized: true, // This is required for static export
   },
-  // Add these lines:
-  basePath: "/portfolio",
+  // Only use basePath for GitHub Pages, not for custom domain
+  ...(isCustomDomain ? {} : { basePath: "/portfolio" }),
   trailingSlash: true,
 };
