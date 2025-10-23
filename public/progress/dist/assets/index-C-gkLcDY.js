2bc538915da1fa415325a7ebffaf1f115eaf8308
ciaranengelbrecht <cizzaengel@gmail.com>
2025-10-23 10:27:23 +0800

    recovery ui

--- public/progress/dist/assets/index-C-gkLcDY.js
+++ public/progress/dist/assets/index-C-gkLcDY.js
@@ -1,5 +1,5 @@
 // Auto-generated legacy alias.
 // Ensures stale clients requesting index-C-gkLcDY.js receive the current bundle.
-import * as mod from "./index-BvWcZLYj.js";
-export * from "./index-BvWcZLYj.js";
+import * as mod from "./index-D1uJjNwn.js";
+export * from "./index-D1uJjNwn.js";
 export default mod;
